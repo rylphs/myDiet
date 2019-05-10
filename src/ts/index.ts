c282abeff235110d0b345428f6dd3e989de44a6c
Raphael <raphael.ferreira@serpro.gov.br>
2019-05-10 18:28:22 -0300

    Commit temporario

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,17 +1,38 @@
 var $:any;
 
 var tabTemplates = [
-    { file: "diario.html", tag: "#tabDiario" }
+    { file: "diario.html", tag: "#tabDiario" }/*,
+    { file: "refeicoes.html", tag: "#tabRefeicoes" }*/
 ]
 
 $.when($.ready).then(function() {
+    
+    
     loadTemplates();
+
+    var service:any = new RegistroService();
+
+    setRefeicaoSelecionada(service.refeicaoDeAgora.id);
+   
+    
+    var data = {
+        refeicaoSelecionada: service.refeicaoDeAgora,
+        refeicoes: service.getTodasAsRefeicoesBase()
+    }
+    $.get("templates/refeicoes.html", function(value:string) {
+        carregarTemplate(value, "#tabRefeicoes", data);
+        $(".hora").timepicki();
+    });
+
+    // $.get("teste.html", function(value:string) {
+    //     carregarTemplate(value, "#tabAlimentos", {});
+    // });
 });
 
 function carregarTemplate(template:any, dst:any, data:any) {
     var tmpl = $.templates(template);
-    var html = tmpl.render(data); // Render template using data - as HTML string
-    $(dst).html(html); // Insert HTML string into DOM
+    var html = tmpl.link(dst, data); // Render template using data - as HTML string
+    //$(dst).html(html); // Insert HTML string into DOM
 }
 
 function loadTemplates() {
@@ -20,6 +41,7 @@ function loadTemplates() {
         refeicaoSelecionada: service.refeicaoDeAgora,
         refeicoes: service.getTodasAsRefeicoesBase()
     }
+    
     for (var i in tabTemplates) {
         var template = tabTemplates[i];
 
@@ -27,4 +49,21 @@ function loadTemplates() {
             carregarTemplate(value, template.tag, data);
         });
     }
-}   
+}
+
+
+
+    function selecionarRefeicao(event:any) {
+        var id = $(event.currentTarget).data("index");
+        setRefeicaoSelecionada(id);
+    }
+
+    function setRefeicaoSelecionada(id:any) {
+        var service:any = new RegistroService();
+        var refeicao = service.getRefeicaoBase(Number(id));
+        $("#iconRefeicaoSelecionada").removeClass();
+        $("#iconRefeicaoSelecionada").addClass("icon " + refeicao.icon);
+        $("#refeicaoSelecionada ").html(refeicao.nome);
+    }
+
+    
